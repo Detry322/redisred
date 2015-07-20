793610e1250ef9353063d7bb4c579b8005630549
Jack Serrino <Jserrino@gmail.com>
2015-07-20 13:15:22 -0700

    Actually sort redirects

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -8,10 +8,10 @@ module.exports = function(redis) {
       if (err)
         res.status(500).send(err);
       else {
-        var sortedRedirects = redirects.sort(function(a,b){
+        redirects.sort(function(a,b){
           return a.key.toLowerCase() > b.key.toLowerCase();
         });
-        res.status(200).render('admin/redirects', { redirects: sortedRedirects, token: req.csrfToken() });
+        res.status(200).render('admin/redirects', { redirects: redirects, token: req.csrfToken() });
       }
     });
   };
