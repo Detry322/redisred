3e829897f6b077221b84f7ce1251cfc25e6f3b4e
Jack Serrino <Jserrino@gmail.com>
2015-08-13 19:01:58 -0700

    Move API to json encoded data

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var csrf = require('csurf');
+var bodyParser = require('body-parser');
 
 module.exports = function(frontend, api) {
 
   var apiRouter = express.Router();
+  apiRouter.use(bodyParser.json());
   apiRouter.get('/', api.authenticate, api.getAllRedirects);
   apiRouter.post('/create', api.authenticate, api.createRedirect);
   apiRouter.post('/delete', api.authenticate, api.deleteRedirect);
 
   var csrfProtection = csrf({ cookie: true });
   var frontendRouter = express.Router();
+  frontendRouter.use(bodyParser.urlencoded({ extended: false }));
   frontendRouter.get('/', frontend.showLogin);
   frontendRouter.post('/login', frontend.login);
   frontendRouter.get('/logout', frontend.logout);
